02628aa15621ae6fb4976432f41b3240578e7189
Mark <hendriksen-mark@hotmail.com>
2024-08-09 01:47:38 +0200

    Update configHandler.py

--- BridgeEmulator/configManager/configHandler.py
+++ BridgeEmulator/configManager/configHandler.py
@@ -149,7 +149,7 @@ class Config:
                     #self.yaml_config["groups"]["0"].add_light(self.yaml_config["lights"][light])
             #groups
             #create group 0
-            self.yaml_config["groups"]["0"] = Group.Group({"name":"Group 0","id_v1": "0", "owner": "6a660825-51cd-5403-937d-4dd8cd43cdf9", "type":"LightGroup","state":{"all_on":False,"any_on":True},"recycle":False,"action":{"on":False,"bri":165,"hue":8418,"sat":140,"effect":"none","xy":[0.6635,0.2825],"ct":366,"alert":"select","colormode":"hs"}})
+            self.yaml_config["groups"]["0"] = Group.Group({"name":"Group 0","id_v1": "0", "owner": str(uuid.uuid5(uuid.NAMESPACE_URL, config["bridgeid"] + 'device')), "type":"LightGroup","state":{"all_on":False,"any_on":True},"recycle":False,"action":{"on":False,"bri":165,"hue":8418,"sat":140,"effect":"none","xy":[0.6635,0.2825],"ct":366,"alert":"select","colormode":"hs"}})
             for key, light in self.yaml_config["lights"].items():
                 self.yaml_config["groups"]["0"].add_light(light)
             # create groups
@@ -167,9 +167,9 @@ class Config:
                                 self.yaml_config["groups"][group].locations[lightObj] = location
                     else:
                         if "owner" in data and isinstance(data["owner"], dict):
-                            data["owner"] = list(self.yaml_config["apiUsers"])[0]
+                            data["owner"] = str(uuid.uuid5(uuid.NAMESPACE_URL, config["bridgeid"] + 'device'))
                         elif "owner" not in data:
-                            data["owner"] = list(self.yaml_config["apiUsers"])[0]
+                            data["owner"] = str(uuid.uuid5(uuid.NAMESPACE_URL, config["bridgeid"] + 'device'))
                         self.yaml_config["groups"][group] = Group.Group(data)
                         for light in data["lights"]:
                             self.yaml_config["groups"][group].add_light(self.yaml_config["lights"][light])
